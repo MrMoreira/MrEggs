1f72f22815f486b76e2b8067a739f8cce29a223a
Caique Moreira <caiquemoreira2525@gmail.com>
2026-06-15 17:54:55 -0300

    xterm

--- Connect/pt-BR/Terraria/launch.sh
+++ Connect/pt-BR/Terraria/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # shellcheck shell=dash
 
+# Fix: Mono TermInfoReader tem limite de 4KB; xterm-256color ultrapassa esse limite.
+# Forçar TERM=xterm (ficheiro terminfo ~3KB) resolve o crash "File must be smaller than 4K".
+export TERM=xterm
+
 if [ "${TERRARIA_VERSION}" = "1.4.4" ] || [ "${TERRARIA_VERSION}" = "144" ]; then
     if [ "${FAKETIME}" = "1" ]; then
         LD_PRELOAD=/usr/lib/faketime/libfaketime.so.1 FAKETIME="$ANO_DIA" mono --server --gc=sgen -O=all ./TerrariaServer.exe -ip 0.0.0.0 -port {{SERVER_PORT}} -maxplayers {{MAX_PLAYERS}} -difficulty {{DIFFICULTY}} -pass {{PASSWORD}} -motd {{MOTD}} -world /home/container/.local/share/Terraria/Worlds/{{WORLD_NAME}}.wld -autocreate {{WORLD_SIZE}} -banlist /home/container/banlist.txt -worldname {{WORLD_NAME}} -secure {{SECURE}} -seed {{SEED}}
